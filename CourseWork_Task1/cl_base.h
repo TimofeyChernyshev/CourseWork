55cc36222d7a651da29676305c99aae2f106d198
TimofeyChernyshev <timkachern@mail.ru>
2025-04-18 17:37:33 +0300

    Create cl_base.h

---
+++ CourseWork_Task1/cl_base.h
@@ -0,0 +1,22 @@
+#ifndef __CL_BASE__H
+#define __CL_BASE__H
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class cl_base{
+	string name; // имя объекта
+	cl_base* head_obj = nullptr; // указатель на родительский объект
+	vector<cl_base*> child; // вектор дочерних объектов
+	public:
+		cl_base(cl_base* head_obj, string name = "Base_object"); // конструктор
+		bool redact_name(string name); // изменяет имя объекта
+		string ret_name(); // возвращает имя объекта
+		cl_base* ret_head_obj(); // вовзращает указатель на головной объект
+		void tree(); // выводит дерево иерархии
+		cl_base* ret_child(string chld_name); // возвращает указатель на дочерний объект
+		~cl_base();
+};
+
+#endif
