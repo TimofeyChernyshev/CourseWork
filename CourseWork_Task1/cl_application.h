5eb3c068c6e0ee224f1aaf1cd56c82fcc0e0d921
TimofeyChernyshev <timkachern@mail.ru>
2025-04-18 17:35:50 +0300

    Create cl_application.h

---
+++ CourseWork_Task1/cl_application.h
@@ -0,0 +1,12 @@
+#ifndef __CL_APPLICATION__H
+#define __CL_APPLICATION__H
+#include "cl_base.h"
+#include "cl_1.h"
+class cl_application: public cl_base{
+	public:
+		cl_application(cl_base* head_obj); // конструктор, создает корневой объект
+		void build_tree_objects(); // создает иерархию объектов
+		int exec_app(); // запускает алгоритм программы
+};
+
+#endif
